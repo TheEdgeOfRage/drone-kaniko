882904715c9c1a27d6bb4390e7e9578737c97b4f
Pavle Portic <git@theedgeofrage.com>
2021-09-18 21:41:21 +0200

    Parametrize caching path and ttl

--- plugin.sh
+++ plugin.sh
@@ -6,17 +6,22 @@ export PATH=$PATH:/kaniko/
 REGISTRY=${PLUGIN_REGISTRY:-index.docker.io}
 CONTEXT=${PLUGIN_CONTEXT:-$PWD}
 DOCKERFILE=${PLUGIN_DOCKERFILE:-Dockerfile}
+CACHE_DIR="--cache-dir=${PLUGIN_CACHE_DIR:-/cache}"
 
 if [[ -n "${PLUGIN_TARGET:-}" ]]; then
     TARGET="--target=${PLUGIN_TARGET}"
 fi
-
 if [ -n "${PLUGIN_BUILD_ARGS:-}" ]; then
     BUILD_ARGS=$(echo "${PLUGIN_BUILD_ARGS}" | tr ',' '\n' | while read build_arg; do echo "--build-arg=${build_arg}"; done)
 fi
-
 if [ -n "${PLUGIN_BUILD_ARGS_FROM_ENV:-}" ]; then
     BUILD_ARGS_FROM_ENV=$(echo "${PLUGIN_BUILD_ARGS_FROM_ENV}" | tr ',' '\n' | while read build_arg; do echo "--build-arg ${build_arg}=$(eval "echo \$$build_arg")"; done)
+fi
+if [[ "${PLUGIN_CACHE:-}" == "true" ]]; then
+    CACHE="--cache --cache-copy-layers"
+fi
+if [ -n "${PLUGIN_CACHE_TTL:-}" ]; then
+    CACHE_TTL="--cache-ttl=${PLUGIN_CACHE_TTL}"
 fi
 
 # auto_tag, if set auto_tag: true, auto generate .tags file
@@ -44,13 +49,12 @@ if [[ "${PLUGIN_AUTO_TAG:-}" == "true" ]]; then
         echo "${major},${major}.${minor},${major}.${minor}.${release},latest" > .tags
     fi
 fi
-
 if [ -n "${PLUGIN_TAGS:-}" ]; then
     DESTINATIONS=$(echo "${PLUGIN_TAGS}" | tr ',' '\n' | while read tag; do echo
-"--destination=${REGISTRY}/${PLUGIN_REPO}:${tag} "; done)
+    "--destination=${REGISTRY}/${PLUGIN_REPO}:${tag} "; done)
 elif [ -f .tags ]; then
     DESTINATIONS=$(cat .tags| tr ',' '\n' | while read tag; do echo
-"--destination=${REGISTRY}/${PLUGIN_REPO}:${tag} "; done)
+    "--destination=${REGISTRY}/${PLUGIN_REPO}:${tag} "; done)
 elif [ -n "${PLUGIN_REPO:-}" ]; then
     DESTINATIONS="--destination=${REGISTRY}/${PLUGIN_REPO}:latest"
 else
@@ -61,15 +65,14 @@ fi
 # echo "{\"auths\":{\"$CI_REGISTRY\":{\"auth\":\"$(echo -n $CI_REGISTRY_USER:$CI_REGISTRY_PASSWORD | base64)\"}}}" > /kaniko/.docker/config.json
 
 set -x
-
 /kaniko/executor \
-	--reproducible \
-	--cache \
-	--cache-copy-layers \
-	--cache-dir /cache \
-	--context ${CONTEXT} \
-	--dockerfile ${DOCKERFILE} \
-	${BUILD_ARGS:-} \
-	${BUILD_ARGS_FROM_ENV:-}
-	${PLUGIN_EXTRA_ARGS:-}
-	${DESTINATIONS} \
+    --reproducible \
+    ${CACHE} \
+    ${CACHE_DIR} \
+    ${CACHE_TTL} \
+    --context ${CONTEXT} \
+    --dockerfile ${DOCKERFILE} \
+    ${BUILD_ARGS:-} \
+    ${BUILD_ARGS_FROM_ENV:-} \
+    ${PLUGIN_EXTRA_ARGS:-} \
+    ${DESTINATIONS} \
